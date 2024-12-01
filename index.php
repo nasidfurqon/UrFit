3c458a4a30254786f8274d47ee8ad6997b3d490e
nasidfurqon <nasidfurqon@gmail.com>
2024-12-01 11:49:06 +0700

    ADD: navbar

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=shopping_cart" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=account_circle" /> -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <title>UrFit</title>
+</head>
+<body>
+    <div class="wrapper">
+        <!-- NAVBAR -->
+        <aside class="nav pt-3 ps-5 pe-5 pb-3">
+            <div class="left">  
+                <a href="index.php" class="text-decoration-none me-5">
+                    <img class="logo" src="/asset/logo.png" alt="logo">
+                </a>
+                <ul class="ms-5 list-unstyled menu-nav">         
+                    <li class="fw-bold menu me-5">
+                        <a href="" class="text-decoration-none text-black">Sport</a>
+                    </li>
+                    <li class="ms-5 fw-bold menu me-5">
+                        <a href="" class="text-decoration-none text-black">Casual</a>
+                    </li>   
+                    <li class="ms-5 fw-bold menu me-5">
+                        <a href="" class="text-decoration-none text-black">Classic</a>
+                    </li>       
+                </ul>
+                <a href="" class="search-icon ms-5 fw-bold text-decoration-none text-dark material-icons fs-3">search</a>
+            
+            </div>
+            <div class="right">
+                <a class="text-dark text-decoration-none" href="">
+                    <i class="material-icons fs-1">
+                        shopping_cart</i>
+                </a>
+                <a href="" class="text-decoration-none text-dark">
+                    <i class="ms-4 fs-1 material-icons">
+                    account_circle
+                    </i>
+                </a>
+            </div>  
+        </aside>
+
+
+        <!-- BODY -->
+    </div>
+</body>
+</html>
