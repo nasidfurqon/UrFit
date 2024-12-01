674e910c3c96409b260abf392d6e46c8734da417
nasidfurqon <nasidfurqon@gmail.com>
2024-12-01 12:51:58 +0700

    ADD: content-1

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
     <link rel="stylesheet" href="style/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=shopping_cart" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=account_circle" /> -->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&family=Caveat&family=Courgette&family=Dancing+Script&family=Exo+2:ital,wght@1,300&family=Handlee&family=Lora:ital,wght@0,400..700;1,400..700&family=Nunito:wght@300&family=Pangolin&family=Quicksand:wght@500&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Shadows+Into+Light&family=Sono:wght@300&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&family=Caveat&family=Courgette&family=Dancing+Script&family=Exo+2:ital,wght@1,300&family=Handlee&family=Nunito:wght@300&family=Pangolin&family=Quicksand:wght@500&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Shadows+Into+Light&family=Sono:wght@300&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>UrFit</title>
 </head>
@@ -48,6 +50,22 @@
 
 
         <!-- BODY -->
+        <div class="content">
+            <div class="content-1 pt-5 d-flex text-center justify-content-center align-items-center">
+                <div class="text-content-1 ">
+                    <h1 class="head-content-1">URFIT</h1>
+                    <h3 class=" head-content-2 pt-3">ALL ABOUT OUTFIT IS HERE</h1>
+                    <div class="button-content mt-5 fw-1">
+
+                        <a href="" class="button text-decoration-none">Sport</a>
+                        <a href="" class="button text-decoration-none">Casual</a>
+                        <a href="" class="button text-decoration-none">Classic</a>
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
